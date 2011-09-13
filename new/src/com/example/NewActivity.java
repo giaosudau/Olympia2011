b45c330d51115e8c3532b0c4d4130eab1a1da44c
sepdau <sepdau@vnexplorer>
2011-09-13 11:04:52 +0700

    11111111111

---
+++ new/src/com/example/NewActivity.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewActivity extends Activity {
+    /** Change !!! */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
